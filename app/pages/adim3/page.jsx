83b4c53cacc4fe25904b99f9be83e9e4a93907a5
Elif <elif.keskin233@gmail.com>
2024-09-29 12:51:56 +0300

    adım 3 form done

--- app/pages/adim3/page.jsx
+++ app/pages/adim3/page.jsx
@@ -2,7 +2,30 @@ export default function adim3(){
 
     return (
         <div>
-       
+        <h1>Öğrenci Kayıt - Adım 3</h1>
+        <form>
+          <p>
+            <label>Dönem:</label>
+            <input type="text" name="term" />
+          </p>
+          <p>
+            <label>Eğitmen:</label>
+            <input type="text" name="teacher" />
+          </p>
+          <p>
+            <label>Bölüm:</label>
+            <input type="text" name="department" />
+          </p>
+          <p>
+            <label>Sınıf:</label>
+            <input type="text" name="class" />
+          </p>
+          <p>
+            <label>Öğrenci Numarası:</label>
+            <input type="number" name="studentNumber" />
+          </p>
+          <a href="/pages/adim2">sonraki sayfa</a> 
+        </form>
       </div>
     );
   }
